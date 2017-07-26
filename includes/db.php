81068124fce2027229c07aff83d98e4c2e6ad770
rungthiprungthip <574407038@parichat.skru.ac.th>
2017-07-26 15:09:24 +0700

    Add test file

--- includes/db.php
+++ includes/db.php
@@ -3,7 +3,7 @@
 $db['db_host'] = "localhost";
 $db['db_user'] = "root";
 $db['db_pass'] = "123456";
-$db['db_name'] = "cms";
+$db['db_name'] = "git";
 
 foreach($db as $key => $value){
 define(strtoupper($key), $value);
